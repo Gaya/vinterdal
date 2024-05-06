4d4724d8a1a8ace2eabc3e7d14dc2b76c9ec4c68
Gaya Kessler <gaya@theclevernode.com>
2024-05-06 20:58:17 +0200

    feat: render roads

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
-import { rrrr } from '~parts/tiles/base.ts';
+import { ffrr, frfr, frrr, rrrr } from '~parts/tiles/base.ts';
 
 import './style.css';
 
 import { tileImage } from './view/tile.ts';
 
 function start() {
-  const img = tileImage(rrrr);
+  const app = document.getElementById('app');
 
-  document.getElementById('app')?.appendChild(img);
+  if (!app) {
+    return;
+  }
+
+  const tiles = [rrrr, frrr, ffrr, frfr];
+
+  tiles.forEach((t) => {
+    app.appendChild(tileImage(t));
+  });
 }
 
 start();
--- src/view/tile.ts
+++ src/view/tile.ts
@@ -1,9 +1,9 @@
-import { Tile } from '~models/tile.ts';
+import { Tile, TileEdge } from '~models/tile.ts';
 
 function make(
   tag: string,
   attributes: Record<string, string | number>,
-  children: SVGElement[] = [],
+  children: (SVGElement | null)[] = [],
 ): SVGElement {
   const element = document.createElementNS('http://www.w3.org/2000/svg', tag);
 
@@ -11,12 +11,70 @@ function make(
     element.setAttribute(key, value.toString());
   });
 
-  children.forEach((child) => element.appendChild(child));
+  children.forEach((child) => child && element.appendChild(child));
 
   return element;
 }
 
+function startRoadByIndex(index: number): [number, number] {
+  let x = 0;
+  let y = 0;
+
+  switch (index) {
+    case 0:
+    case 2:
+      x = 50;
+      break;
+    case 1:
+      x = 100;
+      break;
+    default:
+    case 3:
+      x = 0;
+      break;
+  }
+
+  switch (index) {
+    case 3:
+    case 1:
+      y = 50;
+      break;
+    case 2:
+      y = 100;
+      break;
+    default:
+    case 0:
+      y = 0;
+      break;
+  }
+
+  return [x, y];
+}
+
+function roadPath(roads: [TileEdge, number][]): string {
+  if (roads.length === 4) {
+    return 'M 50,0 L 50,100 M 0,50 L 100,50';
+  }
+
+  if (roads.length === 3) {
+    return roads
+      .map(([_, index]) => {
+        const [x, y] = startRoadByIndex(index);
+
+        return `M ${x},${y} L 50,50`;
+      }).join(' ');
+  }
+
+  return `M ${startRoadByIndex(roads[0][1]).join(',')} C 50,50 50,50 ${startRoadByIndex(roads[1][1]).join(',')}`;
+}
+
 export function tileImage(tile: Tile): SVGElement {
+  const roads: [TileEdge, number][] = tile.edges
+    .map((edge, index): [TileEdge, number] => [edge, index])
+    .filter(([edge]) => edge === TileEdge.ROAD);
+
+  console.log(roads);
+
   return make(
     'svg',
     { viewBox: '0 0 100 100', role: 'img' },
@@ -25,6 +83,15 @@ export function tileImage(tile: Tile): SVGElement {
         'rect',
         { x: '0', y: '0', width: '100', height: '100', fill: 'green' }
       ),
+      roads.length > 0 ? make(
+        'path',
+        {
+          stroke: 'white',
+          'stroke-width': '10',
+          fill: 'none',
+          d: roadPath(roads),
+        }
+      ) : null,
     ]
   );
 }
