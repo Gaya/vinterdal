e047f65a807b8d54cb55806b07c3e47e8163fd95
Gaya Kessler <gaya@theclevernode.com>
2024-05-07 20:19:00 +0200

    feat: attach events

--- src/view/board.ts
+++ src/view/board.ts
@@ -1,8 +1,44 @@
 import { Board } from '~models/board.ts';
+import { playerColor } from '~models/player.ts';
 
 import { tileImage } from './tile.ts';
 import make from './make.ts';
-import { playerColor } from '~models/player.ts';
+
+function claimed(boardPlacement: Board[0]) {
+  if (!boardPlacement.claimed) {
+    return null;
+  }
+
+  const color = playerColor(boardPlacement.claimed.player);
+
+  return make(
+    'svg',
+    {
+      viewBox: '0 0 100 100',
+      class: 'claim',
+    },
+    [
+      make(
+        'path',
+        {
+          stroke: '#000',
+          'stroke-width': 4,
+          fill: color,
+          d: 'm 31.25,35.016 v 19.906 c 0,4.4688 3.625,8.1094 8.0938,8.1094 h 0.3125 l 1.4531,32.344 c 0.03125,0.84375 0.71875,1.5 1.5625,1.5 h 14.656 c 0.84375,0 1.5312,-0.65625 1.5625,-1.5 l 1.4531,-32.344 h 0.3125 c 4.4688,0 8.0938,-3.6406 8.0938,-8.1094 V 35.016 c 0,-4.4688 -3.625,-8.0938 -8.0938,-8.0938 H 39.344 c -4.4688,0 -8.0938,3.625 -8.0938,8.0938 z',
+        },
+      ),
+      make(
+        'path',
+        {
+          stroke: '#000',
+          'stroke-width': 4,
+          fill: color,
+          d: 'm 60.859,13.984 c 0,5.9961 -4.8633,10.859 -10.859,10.859 -5.9957,0 -10.859,-4.8633 -10.859,-10.859 0,-5.9957 4.8633,-10.859 10.859,-10.859 5.9957,0 10.859,4.8633 10.859,10.859',
+        },
+      ),
+    ]
+  )
+}
 
 export function renderBoard(board: Board) {
   const root = document.documentElement;
@@ -30,44 +66,19 @@ export function renderBoard(board: Board) {
     Object.entries(board).map(([key, boardPlacement]) => {
       const [x, y] = key.split(':').map((i) => parseInt(i, 10));
       const { tile, orientation } = boardPlacement.tile;
-      const color = playerColor(boardPlacement.claimed?.player);
 
       return make(
         'div',
         {
           class: `tile ${orientation}`,
-          style: `grid-column-start: ${(x - (minx - 1)) + 1}; grid-row-start: ${(y - (miny - 1)) + 1}`
+          style: `grid-column-start: ${(x - (minx - 1)) + 1}; grid-row-start: ${(y - (miny - 1)) + 1}`,
+          onClick: () => {
+            console.log(boardPlacement);
+          },
         },
         [
           tileImage(tile),
-          boardPlacement.claimed
-            ? make(
-              'svg',
-              {
-                viewBox: '0 0 100 100',
-                class: 'claim',
-              },
-              [
-                make(
-                  'path',
-                  {
-                    stroke: '#000',
-                    'stroke-width': 4,
-                    fill: color,
-                    d: 'm 31.25,35.016 v 19.906 c 0,4.4688 3.625,8.1094 8.0938,8.1094 h 0.3125 l 1.4531,32.344 c 0.03125,0.84375 0.71875,1.5 1.5625,1.5 h 14.656 c 0.84375,0 1.5312,-0.65625 1.5625,-1.5 l 1.4531,-32.344 h 0.3125 c 4.4688,0 8.0938,-3.6406 8.0938,-8.1094 V 35.016 c 0,-4.4688 -3.625,-8.0938 -8.0938,-8.0938 H 39.344 c -4.4688,0 -8.0938,3.625 -8.0938,8.0938 z',
-                  },
-                ),
-                make(
-                  'path',
-                  {
-                    stroke: '#000',
-                    'stroke-width': 4,
-                    fill: color,
-                    d: 'm 60.859,13.984 c 0,5.9961 -4.8633,10.859 -10.859,10.859 -5.9957,0 -10.859,-4.8633 -10.859,-10.859 0,-5.9957 4.8633,-10.859 10.859,-10.859 5.9957,0 10.859,4.8633 10.859,10.859',
-                  },
-                ),
-              ]
-            ) : null,
+          claimed(boardPlacement),
         ],
       );
     }),
--- src/view/make.ts
+++ src/view/make.ts
@@ -1,9 +1,14 @@
 const svgTags: { [tag: string]: boolean } = ['svg', 'rect', 'path']
   .reduce((acc, tag) => ({ ...acc, [tag]: true }), {});
 
+interface MakeAttributes {
+  [attr: string]: string | number | EventListener;
+  onClick: EventListener;
+}
+
 export default function make(
-  tag: string,
-  attributes: Record<string, string | number> = {},
+  tag: keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap,
+  attributes: Partial<MakeAttributes>,
   children: (HTMLElement | SVGElement | null)[] = [],
 ) {
   const element = svgTags[tag]
@@ -11,7 +16,18 @@ export default function make(
     : document.createElement(tag);
 
   Object.entries(attributes).forEach(([key, value]) => {
-    element.setAttribute(key, value.toString());
+    if (typeof value === 'undefined') {
+      return;
+    }
+
+    if (typeof value !== 'function') {
+      element.setAttribute(key, value.toString());
+    }
+
+    if (typeof value === 'function' && key.substring(0, 2) === 'on') {
+      const event = key.substring(2).toLowerCase();
+      element.addEventListener(event, value);
+    }
   });
 
   children.forEach((child) => child && element.appendChild(child));
